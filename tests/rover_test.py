ed4c96952aea90d1d5f0eaae725098132a3ab218
Micheal Nestor <michealnestor@outlook.com>
2025-10-16 19:11:06 +0100

    renamed tests

--- tests/rover_test.py
+++ tests/rover_test.py
@@ -2,11 +2,14 @@ import unittest
 from src.rover import execute
 
 class TestExecute(unittest.TestCase):
-    def test_forward_no_rotation(self):
+    def test_forward_no_turn(self):
         self.assertEqual(execute("M"), "0:1:N")
 
-    def test_forward_twice_no_rotation(self):
+    def test_forward_twice_no_turn(self):
         self.assertEqual(execute("MM"), "0:2:N")
 
-    def test_forward_thrice_no_rotation(self):
+    def test_forward_thrice_no_turn(self):
         self.assertEqual(execute("MMM"), "0:3:N")
+    
+    def test_turn_left(self):
+        self.assertEqual(execute("M"), "0:0:W")
