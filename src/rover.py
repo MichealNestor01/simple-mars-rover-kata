5a67e03a23049de5253291ebd34f7c7fe70b7fb7
Micheal Nestor <michealnestor@outlook.com>
2025-10-16 19:08:04 +0100

    added basic tests for moving forward

--- src/rover.py
+++ src/rover.py
@@ -1,2 +1,8 @@
 def execute(command: str) -> str:
-    return ""
+    x = 0
+    y = 0 
+    direction = ""
+    for char in list(command):
+        if char == "M":
+            x += 1
+    return f"0:{x}:N"
--- tests/rover_test.py
+++ tests/rover_test.py
@@ -4,3 +4,9 @@ from src.rover import execute
 class TestExecute(unittest.TestCase):
     def test_forward_no_rotation(self):
         self.assertEqual(execute("M"), "0:1:N")
+
+    def test_forward_twice_no_rotation(self):
+        self.assertEqual(execute("MM"), "0:2:N")
+
+    def test_forward_thrice_no_rotation(self):
+        self.assertEqual(execute("MMM"), "0:3:N")
