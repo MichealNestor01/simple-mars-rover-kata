93ffe607969d81820643083631c89a26896a733c
Micheal Nestor <michealnestor@outlook.com>
2025-10-16 19:01:07 +0100

    added initial failing test

--- src/rover.py
+++ src/rover.py
@@ -1,6 +1,2 @@
-# super simple python addition function
-def summation(a: int, b: int) -> int:
-    return a + b
-
-if __name__ == "__main__":
-    print(f"1 + 1 = {summation(1, 1)}")
+def execute(command: str) -> str:
+    return ""
